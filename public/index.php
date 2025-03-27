6de5697f394cc4dcc3ebcc7a1cf7ba39b957e2e6
Lacasseusededelire <marieemoukoko@gmail.com>
2025-03-27 04:07:01 +0100

    Ajout valeurs par défaut pour APP_ENV et APP_DEBUG

--- public/index.php
+++ public/index.php
@@ -5,7 +5,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+// Valeurs par défaut si APP_ENV ou APP_DEBUG ne sont pas définis
+$env = $_SERVER['APP_ENV'] ?? 'prod';
+$debug = isset($_SERVER['APP_DEBUG']) ? (bool) $_SERVER['APP_DEBUG'] : false;
+
+$kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
